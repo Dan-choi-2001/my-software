f16ae4bdf17d9cf37f1853a60b9aaeab1ec9cec8
TianHuijun <100184213+TianHuijun@users.noreply.github.com>
2022-12-12 00:58:14 +0700

    Add files via upload

---
+++ mod/fix_all_deprecated_keys.sh
@@ -0,0 +1,12 @@
+sudo apt update
+
+for KEY in $( \
+    apt-key list \
+    | grep -E "(([ ]{1,2}(([0-9A-F]{4}))){10})" \
+    | tr -d " " \
+    | grep -E "([0-9A-F]){8}\b" \
+); do
+    K=${KEY:(-8)}
+    apt-key export $K \
+    | sudo gpg --dearmour -o /etc/apt/trusted.gpg.d/imported-from-trusted-gpg-$K.gpg --yes
+done
